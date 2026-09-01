4056e72192fe6afde03c56975b42ab81e118598e
noobman921 <qq2928361457@163.com>
2026-09-01 11:53:32 +0800

    添加readme内容，修改sign.bat

--- secondary_cpu0_ra8p1/Debug/sign.bat
+++ secondary_cpu0_ra8p1/Debug/sign.bat
@@ -1,11 +1,12 @@
 @echo off
 set MCUBOOT_IMAGE_VERSION=1.1.0
-set MCUBOOT_IMAGE_SIGNING_KEY=D:\Single-chip\e2studio\ws7_xip\bootloader_ra8p1\ra\mcu-tools\MCUboot\root-ec-p256.pem
-set MCUBOOT_APP_BIN_CONVERTER=D:\Single-chip\MySoftware\e2studiofsp6.4\toolchains\gcc_arm\13.2.rel1\bin\arm-none-eabi-objcopy.exe
+set MCUBOOT_IMAGE_SIGNING_KEY=..\..\bootloader_ra8p1\ra\mcu-tools\MCUboot\root-ec-p256.pem
+:: 请修改为对应的工具路径
+set MCUBOOT_APP_BIN_CONVERTER=..\..\..\..\MySoftware\e2studiofsp6.4\toolchains\gcc_arm\13.2.rel1\bin\arm-none-eabi-objcopy.exe
 
 :: Fix SLOT_SIZE for OSPI-based secondary (0x300000 = 3MB)
 powershell -Command "(gc bsp_linker_info.h) -replace 'SLOT_SIZE 0x00030000', 'SLOT_SIZE 0x00300000' | Out-File -encoding ASCII bsp_linker_info.h"
 
-python "D:\Single-chip\e2studio\ws7_xip\bootloader_ra8p1\ra\fsp\src\rm_mcuboot_port\rm_mcuboot_port_sign.py" sign --align 32 --max-align 32 --flash_erase_size_bytes 32768 --confirm --pad-header secondary_cpu0_ra8p1.elf secondary_cpu0_ra8p1.bin.signed
+python "..\..\bootloader_ra8p1\ra\fsp\src\rm_mcuboot_port\rm_mcuboot_port_sign.py" sign --align 32 --max-align 32 --flash_erase_size_bytes 32768 --confirm --pad-header secondary_cpu0_ra8p1.elf secondary_cpu0_ra8p1.bin.signed
 
 pause
--- secondary_cpu0_ra8p1/Debug/sign.sh
+++ secondary_cpu0_ra8p1/Debug/sign.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 export MCUBOOT_IMAGE_VERSION=1.0.0
-export MCUBOOT_IMAGE_SIGNING_KEY="D:/Single-chip/e2studio/ws7_xip/bootloader_ra8p1/ra/mcu-tools/MCUboot/root-ec-p256.pem"
-export MCUBOOT_APP_BIN_CONVERTER="D:/Single-chip/MySoftware/e2studiofsp6.4/toolchains/gcc_arm/13.2.rel1/bin/arm-none-eabi-objcopy.exe"
+export MCUBOOT_IMAGE_SIGNING_KEY="../../bootloader_ra8p1/ra/mcu-tools/MCUboot/root-ec-p256.pem"
+#请修改为对应的工具路径
+export MCUBOOT_APP_BIN_CONVERTER="../../../../../MySoftware/e2studiofsp6.4/toolchains/gcc_arm/13.2.rel1/bin/arm-none-eabi-objcopy.exe"
 
-python "D:/Single-chip/e2studio/ws7_xip/bootloader_ra8p1/ra/fsp/src/rm_mcuboot_port/rm_mcuboot_port_sign.py" sign --align 32 --max-align 32 --flash_erase_size_bytes 32768 --confirm --pad-header secondary_cpu0_ra8p1.elf secondary_cpu0_ra8p1.bin.signed
+python "../../bootloader_ra8p1/ra/fsp/src/rm_mcuboot_port/rm_mcuboot_port_sign.py" sign --align 32 --max-align 32 --flash_erase_size_bytes 32768 --confirm --pad-header secondary_cpu0_ra8p1.elf secondary_cpu0_ra8p1.bin.signed
 
 read -p "Press any key to continue..."
